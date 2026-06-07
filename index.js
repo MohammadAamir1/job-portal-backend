61b49b40d31bb6d5315c59ed65ff0c6757188e6e
mohdamircode <amir113918amir@gmail.com>
2026-06-07 01:29:24 -0700

    create Application Controller, Api routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user_route.js"
 import companyRoute from "./routes/company_route.js"
 import jobRoute from "./routes/job_route.js"
+import applicationRoute from "./routes/application_route.js"
 dotenv.config({});
 
 const app = express();
@@ -28,6 +29,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
     connectDB();
